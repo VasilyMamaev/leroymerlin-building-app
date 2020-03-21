d8bd05e20aca875631d3bf7c4b2bfc1e71e6ae10
VasilyMamaev <mamaev93@mail.ru>
2020-03-21 11:59:32 +0500

    подключен redux, логика расчета

--- src/components/calc/drywall-partition/drywall-partition.jsx
+++ src/components/calc/drywall-partition/drywall-partition.jsx
@@ -5,6 +5,10 @@ import Paper from '@material-ui/core/Paper'
 import ReportIcon from '@material-ui/icons/Report'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import { getCalcAC } from '../../../redux/calc-reducer'
+import { connect } from 'react-redux'
+import { getLastCalc } from '../../../redux/calc-selectors'
+import DrywallPartitionTable from './drywall-partition-table/drywall-partition-table'
 
 const DrywallPartition = React.memo((props) => {
 
@@ -31,7 +35,7 @@ const DrywallPartition = React.memo((props) => {
     },
     heightWall: {
       value: '',
-      label: 'ширина прегородки',
+      label: 'высота прегородки',
       valid: false,
       errorMessage: 'эррор',
       touched: false,
@@ -47,6 +51,8 @@ const DrywallPartition = React.memo((props) => {
     }
   })
 
+  const [isCalculated,setIsCalculated] = useState(false)
+
   const onControlValueChange = (value, controlName) => {
     setFormCotrols({
       ...formControls, 
@@ -57,13 +63,47 @@ const DrywallPartition = React.memo((props) => {
     })
   }
 
-  console.log(formControls)
+  const onSubmitForm = (evt) => {
+    evt.preventDefault()
+    calculateResult(formControls.widthWall.value, formControls.heightWall.value, formControls.doorsInWall.value)
+    setIsCalculated(true)
+  }
+
+  const calculateResult = (widthWall, heightWall, doorsInWall) => {
+    const ww = Number(widthWall)
+    const hw = Number(heightWall)
+    const dw = Number(doorsInWall)
+
+    const doorsArea = dw * 1.2
+    const area = (ww * hw) - doorsArea
+    const perimeter = (ww + hw) * 2
+
+    props.addCalc(
+      {
+        calcName: 'Расчет перегородки из ГКЛ',
+        calcResult: {
+          gkl: Math.ceil(area / 1.5),
+          pn: Math.ceil(perimeter / 3) + (2 * dw),
+          ps: Math.ceil((ww / 0.6) - 1),
+          screws: Math.ceil(area * 68),
+          putty: Math.ceil(area * 1.8),
+          ReinforcingTape: Math.ceil(ww > 1.2 ? (Math.ceil(ww / 0.6) - 2 ) * hw : 0),
+          dowels: Math.ceil(area * 1.5),
+          sealingTape: Math.ceil(perimeter),
+          primer: Math.ceil(area / 5),
+          finishPutty: Math.ceil(area * 2.4),
+          insulation: Math.ceil(area)
+        }
+      }
+    ) 
+  }
+
   return (
     <Container maxWidth="sm" className={classes.container}>
       <h2>Расчет перегородки из ГКЛ</h2>
       <Paper>
         <Paper><ReportIcon></ReportIcon>Внимание! Дробное число вводиться через точку, например 2.5</Paper>
-        <form onSubmit={console.log('')}>
+        <form onSubmit={onSubmitForm}>
           {Object.keys(formControls).map((controlName, index) => {
             return <div>
               <TextField
@@ -83,8 +123,25 @@ const DrywallPartition = React.memo((props) => {
           <Button type='submit' variant='contained' style={{backgroundColor: green[500]}}>Расчитать</Button>
         </form>
       </Paper>
+      { isCalculated ? <DrywallPartitionTable lastCalc={props.lastCalc}/> : null }
     </Container>
   )
 })
 
-export default DrywallPartition
+
+let mapStateToProps = (state) => {
+  return {
+    lastCalc: getLastCalc(state)
+  }
+}
+
+let mapDispatchToProps = (dispatch) => {
+  return {
+    addCalc: (calc) => {
+      dispatch(getCalcAC(calc))
+    }
+  }
+
+}
+
+export default connect(mapStateToProps,mapDispatchToProps) (DrywallPartition)
